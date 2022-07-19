28f0c54f60ad215cdca0f6e36896b8c74b8f6396
O.A.F <>
2022-07-19 15:59:24 +0000

    Daily to get jick jar (#990)
    * Daily to get jick jar
    
    * Update dailies.ts
    
    * prettier

--- scripts/garbage-collector/garbo-combat.js
+++ scripts/garbage-collector/garbo-combat.js
@@ -25543,7 +25543,7 @@ function checkGithubVersion() {
     var mainBranch = gitBranches.find(branchInfo => branchInfo.name === "main");
     var mainSha = mainBranch && mainBranch.commit ? mainBranch.commit.sha : "CustomBuild";
 
-    if ("6c853e235af162fa8a3076f4be20304319f27b3d" === mainSha) {
+    if ("cac8945a09c5d41e1247884cbd292445071f6e25" === mainSha) {
       print("Garbo is up to date!", HIGHLIGHT);
     } else {
       print("Garbo is out of date. Please run 'svn update!", "red");
