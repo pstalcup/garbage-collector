dcae9e2a1f3b30b9833d1a6884a2dbf703aed33b
Patrick Stalcup <legomaniac0117@gmail.com>
2023-09-28 08:16:35 -0400

    Update with garbo utility functions moved into libram

--- src/fights.ts
+++ src/fights.ts
@@ -103,6 +103,7 @@ import {
   have,
   maxBy,
   property,
+  realmAvailable,
   Requirement,
   Robortender,
   set,
@@ -181,7 +182,6 @@ import { bathroomFinance, potionSetup } from "./potions";
 import { garboValue } from "./value";
 import { DraggableFight, WanderOptions } from "./wanderer";
 import { wanderer } from "./garboWanderer";
-import { realmAvailable } from "./wanderer/lib";
 
 const firstChainMacro = () =>
   Macro.if_(
